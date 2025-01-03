9703317327a8bbd3780113ab2b90ad08a673737b
Juhana Pikkarainen <juhana.pikkarainen@gmail.com>
2025-01-03 08:44:08 -0500

    rotation

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
+
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
-def main():
+def main() -> None:
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=(0,0,0))
+        screen.fill(color="black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt += clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
